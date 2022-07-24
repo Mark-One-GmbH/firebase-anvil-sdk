d62aaa5b6a8a0f8971eeb945df98e91b40af0724
mark.breuss@markone.at <mark.breuss@markone.at>
2022-07-24 15:26:18 +0000

    Edited firebase_core.examples

--- client_code/firebase_core/examples.py
+++ client_code/firebase_core/examples.py
@@ -1,23 +1,27 @@
 
+from ..firebase_core import firestore as fs
 
-
+"""Read Data from Firestore"""
 def get_multiple_documents():
-  #get the firestore modules
-  from .. import firebase_core
-  fs = firebase_core.firestore
-  #form query
   query = fs.db.collection('test').where('name','==','hello')
-  #execute query
   docs = fs.get_docs(query)
   print(docs)
   
 
 def get_single_document():
-  #get the firestore modules
-  from .. import firebase_core
-  fs = firebase_core.firestore
-  #get doc reference
   doc_ref = fs.db.collection('test').doc('doc_uid')
-  #execute query
   doc = fs.get_doc(doc_ref)
   print(doc.data)
+  
+  
+  
+  """Write Data"""
+  def set_document():
+    doc_ref = fs.db.collection('test').doc('doc_uid')
+    doc = fs.get_doc(doc_ref)
+    print(doc.data)
+  
+  def get_single_document():
+    doc_ref = fs.db.collection('test').doc('doc_uid')
+    doc = fs.get_doc(doc_ref)
+    print(doc.data)
