a188382012385964ebfb7a88b3e2aa228cdf05eb
mark.breuss@markone.at <mark.breuss@markone.at>
2022-08-10 12:28:14 +0000

    Created readme

---
+++ client_code/readme.py
@@ -0,0 +1,7 @@
+
+'''
+Evaluating a purely firestore user server side:
+https://firebase.google.com/docs/auth/admin/verify-id-tokens#web
+
+With this mechanism a truly serverless anvil app would be possible.
+'''
