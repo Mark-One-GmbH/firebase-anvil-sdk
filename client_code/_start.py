3e46bfee78493530dcc4a3ece43c6e9f3837818c
mark.breuss@markone.at <mark.breuss@markone.at>
2022-09-10 12:36:02 +0000

    Edited _start

--- client_code/_start.py
+++ client_code/_start.py
@@ -5,6 +5,10 @@ from .testing import run_all_tests
 run_all_tests()
 
 
+#import anvil.http
+#resp = anvil.http.request("https://api.mysite.com/foo")
+#print(f"Response MIME type: {resp.content_type}")
+
 
 
 
