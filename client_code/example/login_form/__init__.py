3de0dda979ab282443a9a60cc9ed9736729b23e2
mark.breuss@markone.at <mark.breuss@markone.at>
2022-09-20 19:14:22 +0000

    v0.1

--- client_code/example/login_form/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._anvil_designer import login_formTemplate
-from anvil import *
-
-class login_form(login_formTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
--- client_code/example/test_form/code_snippet/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import code_snippetTemplate
-from anvil import *
-
-class code_snippet(code_snippetTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run when the form opens.
