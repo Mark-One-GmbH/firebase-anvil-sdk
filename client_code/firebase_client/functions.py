2d6df67963c16cd73043f8023660e1bfa4748083
mark.breuss@markone.at <mark.breuss@markone.at>
2023-02-03 10:39:40 +0000

    Edited firebase_client.functions

--- client_code/firebase_client/functions.py
+++ client_code/firebase_client/functions.py
@@ -17,13 +17,13 @@ def init(app,region=None):
 def call(func_name,parameters={}):
   '''calls a cloud function v1'''
   cloud_function = proxy_fs.httpsCallable(functions, func_name);
-  result = cloud_function(parameters)
+  result = cloud_function(utility.to_proxy(parameters))
   return utility.from_proxy(result)
 
 def call_v2(function_url,parameters={}):
   '''calls a cloud function v2 - func_url must be the complete function url!'''
   cloud_function = proxy_fs.httpsCallableFromURL(functions, function_url);
-  result = cloud_function(parameters)
+  result = cloud_function(utility.to_proxy(parameters))
   return utility.from_proxy(result.data)
 
 
