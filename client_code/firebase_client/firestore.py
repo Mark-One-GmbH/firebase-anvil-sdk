7c6d50ec00374822aec6c86fbd89fd9addd49973
mark.breuss@markone.at <mark.breuss@markone.at>
2023-03-15 18:20:51 +0000

    Edited firebase_client.firestore

--- client_code/firebase_client/firestore.py
+++ client_code/firebase_client/firestore.py
@@ -14,7 +14,6 @@ def init(app,enable_offline_cache=False):
   db = proxy_fs.getFirestore(app)
 
   #TODO configure cache size!
-  print(dir(proxy_fs))
   #Offline Caching
   if enable_offline_cache:
     try:
